018c27dfa72e9810b33b206761307cf428b06217
David Andrews <dta@alumni.cmu.edu>
2021-07-03 22:24:36 -0400

    Time initialization and subscription

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Events
 import Browser.Navigation
 import Color
 import Css
@@ -9,6 +10,8 @@ import Date
 import Html.Styled
 import Html.Styled.Attributes
 import Material.Icons.Toggle
+import Result.Extra
+import Task
 import Time
 import TimeZone
 import Url
@@ -19,25 +22,50 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = sub
         , onUrlRequest = always Nop
         , onUrlChange = always Nop
         }
 
 
 type alias Model =
-    { now : Time.Posix
-    , zone : Time.Zone
+    { time : TimeModel
+    , errors : List Error
     }
 
 
+type TimeModel
+    = TimeUninitialized
+        { now : Maybe Time.Posix
+        , zone : Maybe Time.Zone
+        }
+    | TimeInitialized
+        { now : Time.Posix
+        , zone : Time.Zone
+        }
+
+
+type Error
+    = TimeZone TimeZone.Error
+
+
 type Msg
-    = Nop
+    = Error Error
+    | Nop
+    | UpdateNow Time.Posix
+    | UpdateZone Time.Zone
 
 
-init : Int -> Url.Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
-init now _ _ =
-    ( { now = Time.millisToPosix now, zone = TimeZone.america__new_york () }, Cmd.none )
+init : () -> Url.Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
+init _ _ _ =
+    ( { time = TimeUninitialized { now = Nothing, zone = Just (TimeZone.america__new_york ()) }, errors = [] }
+    , TimeZone.getZone
+        |> Task.attempt (Result.Extra.unpack (TimeZone >> Error) (Tuple.second >> UpdateZone))
+    )
+
+
+sub _ =
+    Browser.Events.onAnimationFrame UpdateNow
 
 
 update msg model =
@@ -46,8 +74,46 @@ update msg model =
             ( model, Cmd.none )
     in
     case msg of
+        Error error ->
+            ( { model | errors = model.errors ++ [ error ] }, Cmd.none )
+
         Nop ->
             nop
+
+        UpdateNow posix ->
+            ( { model
+                | time =
+                    case model.time of
+                        TimeUninitialized uninitialized ->
+                            maybeInitialize { uninitialized | now = Just posix }
+
+                        TimeInitialized initialized ->
+                            TimeInitialized { initialized | now = posix }
+              }
+            , Cmd.none
+            )
+
+        UpdateZone zone ->
+            ( { model
+                | time =
+                    case model.time of
+                        TimeUninitialized uninitialized ->
+                            maybeInitialize { uninitialized | zone = Just zone }
+
+                        TimeInitialized initialized ->
+                            TimeInitialized { initialized | zone = zone }
+              }
+            , Cmd.none
+            )
+
+
+maybeInitialize { now, zone } =
+    case ( now, zone ) of
+        ( Just justNow, Just justZone ) ->
+            TimeInitialized { now = justNow, zone = justZone }
+
+        _ ->
+            TimeUninitialized { now = now, zone = zone }
 
 
 view model =
@@ -56,15 +122,16 @@ view model =
         [ Html.Styled.toUnstyled
             (Html.Styled.div
                 []
-                [ Html.Styled.node "link"
+                ([ Html.Styled.node "link"
                     [ Html.Styled.Attributes.href "https://fonts.googleapis.com/css?family=Nunito"
                     , Html.Styled.Attributes.rel "stylesheet"
                     , Html.Styled.Attributes.type_ "text/css"
                     ]
                     []
-                , globalCss
-                , viewBody model
-                ]
+                 , globalCss
+                 ]
+                    ++ viewBody model
+                )
             )
         ]
     }
@@ -86,81 +153,87 @@ globalCss =
         ]
 
 
-viewBody model =
-    let
-        today =
-            Date.fromPosix model.zone model.now
+viewBody { time } =
+    case time of
+        TimeUninitialized _ ->
+            []
 
-        days =
-            List.range 0 5
-                |> List.map (\x -> Date.add Date.Days -x today)
+        TimeInitialized { now, zone } ->
+            let
+                today =
+                    Date.fromPosix zone now
 
-        rows =
-            [ "Row 1"
-            ]
-    in
-    Html.Styled.div
-        [ Html.Styled.Attributes.css
-            [ Css.width (Css.pct 100)
-            , Css.height (Css.pct 100)
-            , Css.overflowX Css.scroll
-            ]
-        ]
-        [ Html.Styled.table
-            [ Html.Styled.Attributes.css
-                [ Css.tableLayout Css.fixed
-                , Css.width Css.zero
-                ]
-            ]
-            (Html.Styled.tr
-                []
-                (Html.Styled.td
-                    [ Html.Styled.Attributes.css [ Css.width (Css.em 8) ]
+                days =
+                    List.range 0 5
+                        |> List.map (\x -> Date.add Date.Days -x today)
+
+                rows =
+                    [ "Row 1"
                     ]
-                    []
-                    :: List.map
-                        (\day ->
-                            Html.Styled.td
-                                [ Html.Styled.Attributes.css
-                                    [ Css.width (Css.em 2)
-                                    , Css.fontVariant Css.smallCaps
-                                    , Css.whiteSpace Css.pre
-                                    , Css.padding (Css.px 8)
-                                    , Css.textAlign Css.center
-                                    ]
-                                ]
-                                [ Html.Styled.text
-                                    (labelForWeekday (Date.weekday day)
-                                        ++ "\n"
-                                        ++ String.fromInt (Date.day day)
-                                    )
-                                ]
+            in
+            [ Html.Styled.div
+                [ Html.Styled.Attributes.css
+                    [ Css.width (Css.pct 100)
+                    , Css.height (Css.pct 100)
+                    , Css.overflowX Css.scroll
+                    ]
+                ]
+                [ Html.Styled.table
+                    [ Html.Styled.Attributes.css
+                        [ Css.tableLayout Css.fixed
+                        , Css.width Css.zero
+                        ]
+                    ]
+                    (Html.Styled.tr
+                        []
+                        (Html.Styled.td
+                            [ Html.Styled.Attributes.css [ Css.width (Css.em 8) ]
+                            ]
+                            []
+                            :: List.map
+                                (\day ->
+                                    Html.Styled.td
+                                        [ Html.Styled.Attributes.css
+                                            [ Css.width (Css.em 2)
+                                            , Css.fontVariant Css.smallCaps
+                                            , Css.whiteSpace Css.pre
+                                            , Css.padding (Css.px 8)
+                                            , Css.textAlign Css.center
+                                            ]
+                                        ]
+                                        [ Html.Styled.text
+                                            (labelForWeekday (Date.weekday day)
+                                                ++ "\n"
+                                                ++ String.fromInt (Date.day day)
+                                            )
+                                        ]
+                                )
+                                days
                         )
-                        days
-                )
-                :: List.map
-                    (\row ->
-                        Html.Styled.tr []
-                            [ Html.Styled.td
-                                [ Html.Styled.Attributes.css
-                                    [ Css.padding (Css.px 8)
-                                    , Css.whiteSpace Css.noWrap
-                                    , Css.textOverflow Css.ellipsis
-                                    ]
-                                ]
-                                [ Html.Styled.span
-                                    [ Html.Styled.Attributes.css
-                                        [ Css.verticalAlign Css.bottom
+                        :: List.map
+                            (\row ->
+                                Html.Styled.tr []
+                                    [ Html.Styled.td
+                                        [ Html.Styled.Attributes.css
+                                            [ Css.padding (Css.px 8)
+                                            , Css.whiteSpace Css.noWrap
+                                            , Css.textOverflow Css.ellipsis
+                                            ]
+                                        ]
+                                        [ Html.Styled.span
+                                            [ Html.Styled.Attributes.css
+                                                [ Css.verticalAlign Css.bottom
+                                                ]
+                                            ]
+                                            [ Html.Styled.fromUnstyled (Material.Icons.Toggle.star Color.black 24) ]
+                                        , Html.Styled.text row
                                         ]
                                     ]
-                                    [ Html.Styled.fromUnstyled (Material.Icons.Toggle.star Color.black 24) ]
-                                , Html.Styled.text row
-                                ]
-                            ]
+                            )
+                            rows
                     )
-                    rows
-            )
-        ]
+                ]
+            ]
 
 
 labelForWeekday weekday =
