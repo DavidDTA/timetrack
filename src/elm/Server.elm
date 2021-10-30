4846c9c339223f00c5d477604472bdf2a4b55896
David Oleander <dta@alumni.cmu.edu>
2021-10-30 18:59:25 -0400

    make format

--- src/elm/Server.elm
+++ src/elm/Server.elm
@@ -3,39 +3,51 @@ port module Server exposing (main)
 import Json.Encode
 import Platform
 
-port requests: ((Json.Encode.Value, Json.Encode.Value) -> msg) -> Sub msg
 
-port responses: (Json.Encode.Value, String) -> Cmd msg
+port requests : (( Json.Encode.Value, Json.Encode.Value ) -> msg) -> Sub msg
+
+
+port responses : ( Json.Encode.Value, String ) -> Cmd msg
+
 
 type alias Model =
-  { authToken : Maybe String
-  ,
-  }
+    { authToken : Maybe String
+    }
 
-type Msg =
-  Request { request: Json.Encode.Value, response: Json.Encode.Value }
 
-error _ = Cmd.none
+type Msg
+    = Request { request : Json.Encode.Value, response : Json.Encode.Value }
 
-main: Platform.Program () () Msg
+
+error _ =
+    Cmd.none
+
+
+main : Platform.Program () () Msg
 main =
     Platform.worker
         { init = init
         , update = update
-        , subscriptions = always (requests (\(req, res) -> Request { request = req, response = res }))
+        , subscriptions = always (requests (\( req, res ) -> Request { request = req, response = res }))
         }
 
+
 init flags =
-  let
-    result = Json.Decode.decodeValue
-      (Json.Decode.at ["auth", "token"] Json.Decode.String)
-      flags
-  in
+    let
+        result =
+            Json.Decode.decodeValue
+                (Json.Decode.at [ "auth", "token" ] Json.Decode.String)
+                flags
+    in
     case result of
-      Ok value -> ({ authToken = Just value }, Cmd.none)
-      Error err -> ({ authToken = Nothing }, error err)
+        Ok value ->
+            ( { authToken = Just value }, Cmd.none )
+
+        Error err ->
+            ( { authToken = Nothing }, error err )
+
 
 update msg model =
-  case msg of
-    Request { request, response } ->
-      (model, responses (response, "Hello from Elm!"))
+    case msg of
+        Request { request, response } ->
+            ( model, responses ( response, "Hello from Elm!" ) )
