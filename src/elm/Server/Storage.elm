f6931f78d68323c017628bbae1daf98627f4edc2
David Oleander <dat@alumni.cmu.edu>
2022-02-06 03:01:59 +0000

    add server

---
+++ src/elm/Server/Storage.elm
@@ -0,0 +1,122 @@
+module Server.Storage exposing (getTimerSet)
+
+import Firestore
+import Firestore.Codec
+import Task
+import Time
+import Timeline
+import TimerSet
+
+
+getTimerSet firestore username =
+    firestore
+        |> Firestore.root
+        |> Firestore.collection "timerSet"
+        |> Firestore.document (pathSafe username)
+        |> Firestore.get (Firestore.Codec.asDecoder timerSetCodec)
+        |> Task.map .fields
+
+
+pathSafe segment =
+    segment
+        |> String.replace "/" "?"
+        |> String.replace "." "?"
+        |> String.replace "_" "?"
+
+
+type alias TimerSetIntermediate =
+    { timerNames : List String
+    , timerActivities : List (Maybe TimerSet.Activity)
+    , timerCategories : List (Maybe TimerSet.Category)
+    , timelineTimestamps : List Time.Posix
+    , timelineTimerIds : List (Maybe TimerSet.TimerId)
+    }
+
+
+timelineFromIntermediate timerNames timerActivities timerCategories timelineTimestamps timelineTimerIds =
+    TimerSet.create
+        (List.map3 TimerSet.Timer timerNames timerActivities timerCategories)
+        (List.map2 Tuple.pair timelineTimestamps timelineTimerIds |> Timeline.fromList)
+
+
+timerSetCodec =
+    let
+        timers timerSet =
+            timerSet
+                |> TimerSet.listTimerIds
+                |> List.filterMap (\id -> TimerSet.get id timerSet)
+
+        listField fieldName listGetter fieldGetter codec =
+            Firestore.Codec.optional fieldName (listGetter >> List.map fieldGetter) (Firestore.Codec.list codec) []
+    in
+    Firestore.Codec.document timelineFromIntermediate
+        |> listField "timerNames" timers .name Firestore.Codec.string
+        |> listField "timerActivities" timers .activity (Firestore.Codec.maybe activityCodec)
+        |> listField "timerCategories" timers .category (Firestore.Codec.maybe categoryCodec)
+        |> listField "timelineTimestamps" (TimerSet.history >> Timeline.toList) Tuple.first Firestore.Codec.timestamp
+        |> listField "timelineTimerIds" (TimerSet.history >> Timeline.toList) Tuple.second (Firestore.Codec.maybe timerIdCodec)
+        |> Firestore.Codec.build
+
+
+timerIdCodec =
+    Firestore.Codec.map TimerSet.timerIdFromRaw TimerSet.timerIdToRaw Firestore.Codec.int
+
+
+activityCodec =
+    Firestore.Codec.int
+        |> Firestore.Codec.andThen
+            (\int ->
+                case int of
+                    0 ->
+                        Firestore.Codec.succeed TimerSet.Active
+
+                    1 ->
+                        Firestore.Codec.succeed TimerSet.Reactive
+
+                    2 ->
+                        Firestore.Codec.succeed TimerSet.Proactive
+
+                    _ ->
+                        Firestore.Codec.fail "invalid activity"
+            )
+            (\activity ->
+                case activity of
+                    TimerSet.Active ->
+                        0
+
+                    TimerSet.Reactive ->
+                        1
+
+                    TimerSet.Proactive ->
+                        2
+            )
+
+
+categoryCodec =
+    Firestore.Codec.int
+        |> Firestore.Codec.andThen
+            (\int ->
+                case int of
+                    0 ->
+                        Firestore.Codec.succeed TimerSet.Operational
+
+                    1 ->
+                        Firestore.Codec.succeed TimerSet.Helpful
+
+                    2 ->
+                        Firestore.Codec.succeed TimerSet.Productive
+
+                    _ ->
+                        Firestore.Codec.fail "invalid codec"
+            )
+            (\category ->
+                case category of
+                    TimerSet.Operational ->
+                        0
+
+                    TimerSet.Helpful ->
+                        1
+
+                    TimerSet.Productive ->
+                        2
+            )
